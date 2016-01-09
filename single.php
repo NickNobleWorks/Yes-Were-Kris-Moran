756ca9500bf19994d86a47c394052cb33a4fa8dc
Nick Noble <nickynoble@me.com>
2016-01-09 03:26:51 -0500

    remove sidebars

--- single.php
+++ single.php
@@ -8,7 +8,6 @@
  */
 
 get_header(); ?>
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -31,5 +30,4 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
