16e7527ce0636389731e6a345c679a646f559f69
Nick Noble <nickynoble@me.com>
2016-03-12 12:08:11 -0500

    move projects post type to plugin

--- functions.php
+++ functions.php
@@ -116,69 +116,6 @@ add_action( 'after_setup_theme', 'km_content_width', 0 );
 // add_action( 'widgets_init', 'km_widgets_init' );
 
 /**
- * Add "Project" post-type.
- */
-
-function project_init() {
-	register_post_type( 'project', array(
-		'labels'            => array(
-			'name'                => __( 'Projects', 'km' ),
-			'singular_name'       => __( 'Project', 'km' ),
-			'all_items'           => __( 'All Projects', 'km' ),
-			'new_item'            => __( 'New project', 'km' ),
-			'add_new'             => __( 'Add New', 'km' ),
-			'add_new_item'        => __( 'Add New project', 'km' ),
-			'edit_item'           => __( 'Edit project', 'km' ),
-			'view_item'           => __( 'View project', 'km' ),
-			'search_items'        => __( 'Search projects', 'km' ),
-			'not_found'           => __( 'No projects found', 'km' ),
-			'not_found_in_trash'  => __( 'No projects found in trash', 'km' ),
-			'parent_item_colon'   => __( 'Parent project', 'km' ),
-			'menu_name'           => __( 'Projects', 'km' ),
-		),
-		'public'            => true,
-		'hierarchical'      => false,
-		'show_ui'           => true,
-		'show_in_nav_menus' => true,
-		'supports'          => array( 'title', 'editor', 'thumbnail', 'revisions' ),
-		'has_archive'       => true,
-		'rewrite'           => true,
-		'query_var'         => true,
-		'menu_position'			=> 5,
-		'menu_icon'         => 'dashicons-images-alt2',
-	) );
-
-}
-add_action( 'init', 'project_init' );
-
-function project_updated_messages( $messages ) {
-	global $post;
-
-	$permalink = get_permalink( $post );
-
-	$messages['project'] = array(
-		0 => '', // Unused. Messages start at index 1.
-		1 => sprintf( __('Project updated. <a target="_blank" href="%s">View project</a>', 'km'), esc_url( $permalink ) ),
-		2 => __('Custom field updated.', 'km'),
-		3 => __('Custom field deleted.', 'km'),
-		4 => __('Project updated.', 'km'),
-		/* translators: %s: date and time of the revision */
-		5 => isset($_GET['revision']) ? sprintf( __('Project restored to revision from %s', 'km'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
-		6 => sprintf( __('Project published. <a href="%s">View project</a>', 'km'), esc_url( $permalink ) ),
-		7 => __('Project saved.', 'km'),
-		8 => sprintf( __('Project submitted. <a target="_blank" href="%s">Preview project</a>', 'km'), esc_url( add_query_arg( 'preview', 'true', $permalink ) ) ),
-		9 => sprintf( __('Project scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview project</a>', 'km'),
-		// translators: Publish box date format, see http://php.net/date
-		date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( $permalink ) ),
-		10 => sprintf( __('Project draft updated. <a target="_blank" href="%s">Preview project</a>', 'km'), esc_url( add_query_arg( 'preview', 'true', $permalink ) ) ),
-	);
-
-	return $messages;
-}
-add_filter( 'post_updated_messages', 'project_updated_messages' );
-
-
-/**
  * Enqueue scripts and styles.
  */
 function km_scripts() {
